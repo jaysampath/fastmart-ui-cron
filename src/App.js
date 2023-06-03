46f6840597b03d575b92ba99134bc803991c94f4
Jay <50858217+jaysampath@users.noreply.github.com>
2023-06-03 15:08:42 +0530

    cron job to hit backend api periodically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const min = 1;
+  const cronIntervalInMillis = min * 60 * 1000;
+  const hitBackendAPi = async () => {
+    console.log("Hitting backend api periodically.");
+    const response = await fetch(
+      "https://fastmart-backend.onrender.com/user/check/invalidmail@email.com"
+    );
+    const data = await response.json();
+  };
+  const cronJob = () => {
+    setInterval(() => {
+      hitBackendAPi();
+    }, cronIntervalInMillis);
+  };
+
+  useEffect(() => {
+    cronJob();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
