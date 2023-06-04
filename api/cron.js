9750c319e3640faaf0977f724fe3192d8aca3f81
Jay <50858217+jaysampath@users.noreply.github.com>
2023-06-04 19:05:04 +0530

    try catch block. Switch to fetch call. format code

--- api/cron.js
+++ api/cron.js
@@ -1,15 +1,25 @@
-
 export default function handler(req, res) {
-    res.status(200).end(JSON.stringify(hitBackendAPi()));
+  try {
+    hitBackendAPi();
+    res.status(200).json({ success: true });
+  } catch (e) {
+    res.status(500).end("Something went wrong while hitting the api");
   }
+}
 
 const hitBackendAPi = async () => {
-    console.log("Hitting backend api periodically using Vercel cron jobs feature");
-    console.log("Current hit time "+ new Date().getTime().toLocaleString);
-    const response = await fetch(
-      "https://fastmart-backend.onrender.com/user/check/invalidmail@email.com"
-    );
-    const data = await response.json();
-    console.log("api response - "+ JSON.stringify(data));
-    return data;
+  console.log(
+    "Hitting backend api periodically using Vercel cron jobs feature"
+  );
+  console.log("Current hit time " + new Date().getTime().toLocaleString());
+  fetch(
+    "https://fastmart-backend.onrender.com/user/check/invalidmail@email.com"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("api response- " + JSON.stringify(data));
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
