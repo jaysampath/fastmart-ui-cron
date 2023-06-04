fff6077e5355df8badbe88f9422df62c9b599fa2
Jay <50858217+jaysampath@users.noreply.github.com>
2023-06-04 18:52:06 +0530

    hit api and get repsonse via cron job

--- api/cron.js
+++ api/cron.js
@@ -5,10 +5,11 @@ export default function handler(req, res) {
 
 const hitBackendAPi = async () => {
     console.log("Hitting backend api periodically using Vercel cron jobs feature");
-    console.log("Current hit time "+ new Date().getTime());
+    console.log("Current hit time "+ new Date().getTime().toLocaleString);
     const response = await fetch(
       "https://fastmart-backend.onrender.com/user/check/invalidmail@email.com"
     );
     const data = await response.json();
+    console.log("api response - "+ JSON.stringify(data));
     return data;
 };
